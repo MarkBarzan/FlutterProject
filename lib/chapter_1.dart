63c794b258b07bd630a91de20281e935a8666b4e
Mark Barzan <mark.barzan@endava.com>
2021-10-28 13:46:00 +0300

    Chapter 1 Solutions

--- lib/chapter_1.dart
+++ lib/chapter_1.dart
@@ -1,3 +1,70 @@
-void main(){
+import 'dart:math';
 
+void main() {
+  /* Mini-exercise 1
+  var sin45 = sin(45 * pi / 180);
+  print(sin45);
+  var equalOfSin45 = 1 / sqrt(2);
+  print(equalOfSin45);
+  */
+
+  /* Mini-exercise 2
+  int myAge = 25;
+  double averageAge = 25;
+  averageAge = (myAge + 29) / 2;
+  print(averageAge);
+  */
+
+  /* Mini-exercise 3
+  const testNumber = 5;
+  const evenOdd = testNumber % 2;
+  print(evenOdd);
+   */
+
+  /* Challenge 1
+  const int c1MyAge = 25;
+  int myDogs = 0;
+  myDogs += 1;
+  print(myDogs);
+   */
+
+  /* Challenge 2
+  int age = 16;
+  print(age);
+  age = 30;
+  print(age);
+  // Added int out of "var, int, const and final". Var works as well; Const and final do not work due to the sound null safety.
+   */
+
+  /* Challenge 3
+  const x = 46;
+  const y = 10;
+
+  const answer1 = (x * 100) + y; // 4610
+  const answer2 = (x * 100) + (y * 100); // 5600
+  const answer3 = (x * 100) + (y / 10); // 4601
+  print(answer1);
+  print(answer2);
+  print(answer3);
+
+   */
+
+  /* Challenge 4
+  const double rating1 = 1;
+  const double rating2 = 3;
+  const double rating3 = 5;
+  const averageRating = (rating1 + rating2 + rating3) / 3;
+  print(averageRating);
+   */
+
+  /* Challenge 5
+  const double a = 1;
+  const double b = -2;
+  const double c = 1;
+  double x = (-b + sqrt((b * b) - (4 * a * c))) / (2 * a);
+
+  print(x);
+
+  // Nu am inteles aici cum pot sa pun in constant root1 si root2 variabila care nu e constanta (double x)
+  */
 }
